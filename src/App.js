fdd83669b7674b0073d9f6f90d829407967efe88
Lucy Suddenly <suddenlylucy@gmail.com>
2019-09-05 14:12:21 -0700

    added react-bootstrap and basic navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {withRouter} from 'react-router';
 import './App.css';
 import Home from './components/containers/Main.js'
+import Navbar from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
-    <div className="App">
-
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" render={() =>(<Home/>)}/>
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <>
+      <Navbar sticky="top" variant="dark" bg="dark">
+        <a href="/">
+          <Navbar.Text>
+            NewPets
+          </Navbar.Text>
+        </a>
+        <Navbar.Collapse className="justify-content-end">
+          <Button>
+            Login or Sign Up
+          </Button>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" render={() =>(<Home/>)}/>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
-export default App;
+export default withRouter(App);
