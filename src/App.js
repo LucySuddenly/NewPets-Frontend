2d9e34bd25e0a60905b9b262efa628e438f8ae72
Lucy Suddenly <suddenlylucy@gmail.com>
2019-09-10 14:32:37 -0700

    added tailwind

--- src/App.js
+++ src/App.js
@@ -4,22 +4,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="text-center ">
+  <header className="bg-purple-900 m-6 p-6 rounded shadow-lg">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1 className="text-white text-3xl">Welcome to React</h1>
+  </header>
+  <p className="text-base">
+    To get started, edit <code>src/App.js</code> and save to reload.
+  </p>
+</div>
   );
 }
 
