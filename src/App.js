e87c5d64ac3eecb8e34b75f0990fb77ba43ac9e8
Lucy Suddenly <suddenlylucy@gmail.com>
2019-09-19 11:02:13 -0700

    changed class to classname, added Login render

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
+import Login from './components/Login'
 
 class App extends Component {
 
   render() {
     return (
       <>
-      <nav class="flex items-center justify-between flex-wrap bg-purple-500 p-4">
-        <div class="flex items-center flex-shrink-0 text-white mr-6">
-          <span class="font-bold text-xl tracking-tight">PixelPets</span>
+      <nav className="flex items-center justify-between flex-wrap bg-purple-500 p-4">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <span className="font-bold text-xl tracking-tight">PixelPets</span>
         </div>
-        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div class="text-sm lg:flex-grow">
-            <a href="/explore" class="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white mr-4">
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow">
+            <a href="/explore" className="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white mr-4">
               Explore
             </a>
-            <a href="/arcade" class="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white mr-4">
+            <a href="/arcade" className="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white mr-4">
               Arcade
             </a>
-            <a href="/news" class="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white">
+            <a href="/news" className="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white">
               News
             </a>
           </div>
           <div>
-            <a href="/login" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Login</a>
+            <a href="/login" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-purple-500 hover:bg-white mt-4 lg:mt-0">Login</a>
           </div>
         </div>
       </nav>
@@ -34,7 +35,7 @@ class App extends Component {
           <Route exact path="/explore"/>
           <Route exact path="/arcade"/>
           <Route exact path="/news"/>
-          <Route exact path="/login"/>
+          <Route exact path="/login" render={() =>(<Login/>)}/>
         </Switch>
       </Router>
       </>
