e8929cdd1333b0f443ed7017c332280634e2c22b
Lucy Suddenly <suddenlylucy@gmail.com>
2019-09-19 10:24:11 -0700

    added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-<div className="text-center ">
-  <header className="bg-purple-900 m-6 p-6 rounded shadow-lg">
-    <img src={logo} className="App-logo" alt="logo" />
-    <h1 className="text-white text-3xl">Welcome to React</h1>
-  </header>
-  <p className="text-base">
-    To get started, edit <code>src/App.js</code> and save to reload.
-  </p>
-</div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <nav class="flex items-center justify-between flex-wrap bg-purple-500 p-4">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <span class="font-bold text-xl tracking-tight">PixelPets</span>
+        </div>
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div class="text-sm lg:flex-grow">
+            <a href="/explore" class="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white mr-4">
+              Explore
+            </a>
+            <a href="/arcade" class="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white mr-4">
+              Arcade
+            </a>
+            <a href="/news" class="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white">
+              News
+            </a>
+          </div>
+          <div>
+            <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Login</a>
+          </div>
+        </div>
+      </nav>
+    )
+  }
 }
 
 export default App;
